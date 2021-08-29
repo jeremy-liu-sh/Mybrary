dea3c7011ec10c2a34005d3ee921c3b154a1e2f3
Jeremy Liu <jeremy.dot.l@gmail.com>
2021-08-29 21:31:56 +0800

    Initial setup.

---
+++ routes/index.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const { builtinModules } = require('module')
+const router = express.Router()
+
+router.get('/', (req, res) => {
+    res.render('index')
+})
+
+module.exports = router
